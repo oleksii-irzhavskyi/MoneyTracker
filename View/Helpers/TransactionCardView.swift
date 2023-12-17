f5c13bfe9ea3bb4711b074966eb1da2f7f0deae0
Oleksii <oleksii.irzhavskyi@gmail.com>
2023-12-18 00:46:05 +0200

    swiftdata

--- View/Helpers/TransactionCardView.swift
+++ View/Helpers/TransactionCardView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct TransactionCardView: View {
+    @Environment(\.modelContext) private var context
     var transaction: Transaction
     var body: some View {
         SwipeAction(cornerRadius: 10, direction: .trailing) {
@@ -42,13 +43,13 @@ struct TransactionCardView: View {
             .background(.background, in: .rect(cornerRadius: 10))
         } actions: {
             Action(tint: .red, icon: "trash") {
-                ///TODO: Later
+                context.delete(transaction)
             }
         }
-
+        
     }
 }
 
 #Preview {
-    TransactionCardView(transaction: sampleTransactions[0])
+    ContentView()
 }
